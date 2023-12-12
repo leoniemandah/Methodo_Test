2be4f513bbaceec69c1a946b976d4fe27200bdcf
Ayoub-Tehari <ayoub.tehari01@gmail.com>
2023-12-12 15:37:54 +0100

    delete extra files

--- src/langue.interface.ts
+++
@@ -1,9 +0,0 @@
-import {MomentDeLaJournée} from "./momentDeLaJournée";
-
-export interface LangueInterface {
-    Féliciter(): string;
-
-    Saluer(moment: MomentDeLaJournée): string;
-
-    Acquitter(): string;
-}
